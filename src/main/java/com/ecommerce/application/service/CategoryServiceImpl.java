b8546390e7c743379e3ba8ed51bf507aa2f43615
Ayush Jain <jnayush10@gmail.com>
2024-12-31 11:21:57 +0530

    added repository layer in the source code and also optmized existing code base

--- src/main/java/com/ecommerce/application/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/application/service/CategoryServiceImpl.java
@@ -1,54 +1,46 @@
 package com.ecommerce.application.service;
 
 import com.ecommerce.application.model.Category;
+import com.ecommerce.application.repositories.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private final List<Category> categories = new ArrayList<>();
-    private long nextID = 1L;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextID++);
-        categories.add(category);
-
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
+        Category storedCategory = categoryRepository
+                .findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id " + categoryId + " does not exist."));
-
-        categories.remove(category);
+        categoryRepository.delete(storedCategory);
         return "Category with id " + categoryId + " deleted successfully !!";
     }
 
     @Override
     public String updateCategory(long categoryId, Category category) {
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if (optionalCategory.isPresent()) {
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return "Category with id " + categoryId + " updated successfully !!";
-        }
-        else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id " + categoryId + " does not exist.");
-        }
+        categoryRepository
+                .findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with id " + categoryId + " does not exist."));
+        category.setCategoryId(categoryId);
+        categoryRepository.save(category);
+        return "Category with id " + categoryId + " updated successfully !!";
     }
 }
