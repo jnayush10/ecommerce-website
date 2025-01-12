e3a5ad5584ef07852b80a65d7fe8684b729fce73
Ayush Jain <jnayush10@gmail.com>
2025-01-12 13:08:15 +0530

    implemented create and get operation on product entity

--- src/main/java/com/ecommerce/application/service/ProductService.java
+++ src/main/java/com/ecommerce/application/service/ProductService.java
@@ -1,4 +1,11 @@
 package com.ecommerce.application.service;
 
+import com.ecommerce.application.model.Product;
+import com.ecommerce.application.payload.ProductDTO;
+import com.ecommerce.application.payload.ProductResponse;
+
 public interface ProductService {
+    ProductDTO addProduct(long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
