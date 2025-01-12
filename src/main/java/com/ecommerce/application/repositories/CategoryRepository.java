9cc66bcb4356d78d6f4222d77c0008a226f4ac78
Ayush Jain <jnayush10@gmail.com>
2025-01-12 22:32:42 +0530

    implemented delete operation for product entity

--- src/main/java/com/ecommerce/application/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/application/repositories/CategoryRepository.java
@@ -1,11 +1,8 @@
 package com.ecommerce.application.repositories;
 
 import com.ecommerce.application.model.Category;
-import com.ecommerce.application.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
