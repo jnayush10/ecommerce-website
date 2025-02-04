7802c3f6ce1b5511885df481566c2ef1bf409a94
Ayush Jain <jnayush10@gmail.com>
2025-02-04 09:53:24 +0530

    Few code improvement changes in address, prodcut, role and user entity

--- src/main/java/com/ecommerce/application/model/Product.java
+++ src/main/java/com/ecommerce/application/model/Product.java
@@ -11,17 +11,21 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long productId;
+
     @NotBlank
     @Size(min = 3, message = "Product name must contain at least 3 characters")
     private String productName;
+
     @NotBlank
     @Size(min = 6, message = "Product description must contain at least 6 characters")
     private String productDescription;
+
     private String image;
     private int quantity;
     private double price;
